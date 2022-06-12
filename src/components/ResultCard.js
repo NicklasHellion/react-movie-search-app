9b29bcf825a4c458958144ba50c680a8a127b851
NicklasHellion <nicola.raduazzo@gmail.com>
2022-06-12 18:20:55 +0200

    final version

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import Moment from "react-moment";
+import { GlobalContext } from "../context/GlobalState";
 
 export const ResultCard = ({ movie }) => {
+  const { addMovieToWatchlist, addMovieToWatched, watchlist, watched } =
+    useContext(GlobalContext);
+
+  let storedMovie = watchlist.find((o) => o.id === movie.id);
+  let storedMovieWatched = watched.find((o) => o.id === movie.id);
+
+  const watchlistDisabled = storedMovie
+    ? true
+    : storedMovieWatched
+    ? true
+    : false;
+
+  const watchedDisabled = storedMovieWatched ? true : false;
+
   return (
     <div className="result-card">
       <div className="poster-wrapper">
@@ -10,7 +26,7 @@ export const ResultCard = ({ movie }) => {
             alt={`${movie.title} Poster`}
           />
         ) : (
-          <div className="filler-poster"></div>
+          <div className="filler-poster" />
         )}
       </div>
 
@@ -18,12 +34,26 @@ export const ResultCard = ({ movie }) => {
         <div className="header">
           <h3 className="title">{movie.title}</h3>
           <h4 className="release-date">
-            {movie.release_date ? movie.release_date.substring(0, 4) : "-"}
+            <Moment format="YYYY">{movie.release_date}</Moment>
           </h4>
         </div>
 
         <div className="controls">
-          <button className="btn">Add to Watchlist</button>
+          <button
+            className="btn"
+            disabled={watchlistDisabled}
+            onClick={() => addMovieToWatchlist(movie)}
+          >
+            Add to Watchlist
+          </button>
+
+          <button
+            className="btn"
+            disabled={watchedDisabled}
+            onClick={() => addMovieToWatched(movie)}
+          >
+            Add to Watched
+          </button>
         </div>
       </div>
     </div>
